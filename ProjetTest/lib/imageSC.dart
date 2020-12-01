2d4eddd666e5de43fe05875e55e33cc98b2b6fd0
Abdelaziz <abdelaziz.elrharbi@outlook.fr>
2020-12-01 14:10:42 +0100

    layout imageSC

--- ProjetTest/lib/imageSC.dart
+++ ProjetTest/lib/imageSC.dart
@@ -17,14 +17,13 @@ class ImageContactLess extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [ 
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
+                  margin: const EdgeInsets.only(top: 150),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text('Montant à régler : '+montant,style: TextStyle(fontFamily: 'Roboto',fontSize: 40,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold)),
-                        SizedBox(width:10),
                         Icon(
                           Icons.euro,
                           size: 50.0,
@@ -33,18 +32,24 @@ class ImageContactLess extends StatelessWidget {
                   ),
                 ),
                 Container(
-                  child: IconButton(
-                    iconSize: 350.0,
-                    icon: Icon(
-                      Icons.contactless_outlined
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Paiement()),
-                      );
-                    },
-                  )
+                  margin: const EdgeInsets.only(top: 70),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children : [
+                      IconButton(
+                        iconSize: 350.0,
+                        icon: Icon(
+                          Icons.contactless_outlined
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Paiement()),
+                          );
+                        },
+                      )
+                    ]
+                  ),
                 )
               ],
             ),
